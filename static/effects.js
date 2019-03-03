0037e30f9be29445e1910bf2091210263b992e05
Thomas <thomasdriscoll98@gmail.com>
2019-03-03 09:37:48 -0600

    not working put trying to get Javascript to POST and GET data requests from scraping.py

--- static/effects.js
+++ static/effects.js
@@ -1,5 +1,27 @@
-$('#searchbox').keyup(function(event)){
-  if (event.keyCode == 13){
-    $('#searchbutton').click()
-  }
+
+
+
+function search() {
+  var stock_type = document.getElementById("searchbox").value
+  var data_obj = {};
+  data_obj.timestamp = $('#timestamp').val();
+  data_obj.open = $('#open').val();
+  data_obj.high = $('#high').val();
+  data_obj.low = $('#low').val();
+  data_obj.close = $('#close').val();
+  data_obj.volume = $('#volume').val();
+  $.ajax({
+    // send data to scraping.py
+    url: 'scraping.py',
+    method: 'POST',
+    data: JSON.stringify(data_obj)
+    dataType: "json",
+    success: function(response){
+      console.log(response);
+    },
+    error: function(response){
+      console.log(response);
+    }
+    // store JSON data and send it to textarea
+  });
 }
