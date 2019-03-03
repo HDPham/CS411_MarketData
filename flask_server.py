abcfe02baf3f38acf5efe18322828fd36782ed6d
Thomas <thomasdriscoll98@gmail.com>
2019-03-03 14:44:38 -0600

    enter triggers call to alpha vantage and hard-coded stock successfully returns; currently working on passing actual stock type

--- flask_server.py
+++ flask_server.py
@@ -6,3 +6,15 @@ app = Flask(__name__, template_folder='templates')
 @app.route('/home')
 def home ():
     return render_template('/home.html')
+
+@app.route('/scraper', methods=['GET', 'POST'])
+def get_stock():
+    from alpha_vantage.timeseries import TimeSeries
+    from bs4 import BeautifulSoup
+    from html.parser import HTMLParser
+    import requests
+
+    ts = TimeSeries(key='IK798ICZ6BMU2EZM', output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+    json_data= data.to_json(orient='split')
+    return json_data
--- static/effects.js
+++ static/effects.js
@@ -1,27 +1,17 @@
-
-
-
 function search() {
-  var stock_type = document.getElementById("searchbox").value
-  var data_obj = {};
-  data_obj.timestamp = $('#timestamp').val();
-  data_obj.open = $('#open').val();
-  data_obj.high = $('#high').val();
-  data_obj.low = $('#low').val();
-  data_obj.close = $('#close').val();
-  data_obj.volume = $('#volume').val();
+  var stock = document.getElementById("searchbox").value;
+  console.log(stock)
   $.ajax({
     // send data to scraping.py
-    url: 'scraping.py',
-    method: 'POST',
-    data: JSON.stringify(data_obj)
+    url: '/scraper',
+    type: 'GET',
+    data: stock,
     dataType: "json",
     success: function(response){
-      console.log(response);
+      document.getElementById("stock_output").value = Object.values(response);
     },
     error: function(response){
-      console.log(response);
+      document.getElementById("stock_output").value = "Sorry, something went wrong in your search :/";
     }
-    // store JSON data and send it to textarea
   });
 }
